52b4e095c10232f344c8c7652b58fce42bf005de
Sven Bartscher <sven.bartscher@credativ.de>
2022-09-29 10:20:38 +0200

    Implement some relative navigation functions to Nodes nodes in base

--- src/base/node.rs
+++ src/base/node.rs
@@ -1,7 +1,7 @@
 #[cfg(doc)]
 use super::*;
 
-use crate::base::iters::{DevTreeIter, DevTreeNodePropIter};
+use crate::base::iters::{DevTreeIter, DevTreeNodeIter, DevTreeNodePropIter};
 use crate::error::Result;
 
 /// A handle to a Device Tree Node within the device tree.
@@ -41,5 +41,66 @@ impl<'a, 'dt: 'a> DevTreeNode<'a, 'dt> {
     /// TODO
     pub fn find_next_compatible_node(&self, string: &str) -> Result<Option<DevTreeNode<'a, 'dt>>> {
         self.parse_iter.clone().next_compatible_node(string)
+    }
+
+    /// Return an iterator over all descendants of this node.
+    pub fn descendants(&self) -> DevTreeIter<'a, 'dt> {
+        let mut i = self.parse_iter.clone();
+        i.limit_to_descendants();
+        i
+    }
+
+    /// Return an iterator over all descendant nodes of this node.
+    pub fn descendant_nodes(&self) -> DevTreeNodeIter<'a, 'dt> {
+        DevTreeNodeIter(self.descendants())
+    }
+
+    /// Return and iterator over all direct children of this node.
+    pub fn children(&self) -> DevTreeIter<'a, 'dt> {
+        let mut i = self.parse_iter.clone();
+        i.limit_to_children();
+        i
+    }
+
+    /// Return an interator over all direct children nodes of this node.
+    pub fn child_nodes(&self) -> DevTreeNodeIter<'a, 'dt> {
+        DevTreeNodeIter(self.children())
+    }
+
+    /// Return an iterator over all descendants of this node, all
+    /// siblings after the node and their descendants. If you only
+    /// want siblings and their descendants, but not this node's
+    /// descendants, first use [`next_sibling`](Self::next_sibling) and then
+    /// [`siblings_and_descendants`](Self::siblings_and_descendants) on that.
+    pub fn siblings_and_descendants(&self) -> DevTreeIter<'a, 'dt> {
+        let mut i = self.parse_iter.clone();
+        i.limit_to_siblings_and_descendants();
+        i
+    }
+
+    /// Return and iterator over all descendant nodes and following
+    /// siblings nodes and their descendant nodes.
+    pub fn sibling_and_descendant_nodes(&self) -> DevTreeNodeIter<'a, 'dt> {
+        DevTreeNodeIter(self.siblings_and_descendants())
+    }
+
+    /// Return an iterator over all later siblings of this node. Note
+    /// that this is dependant on the order of the siblings in the
+    /// DevTree as this can only return siblings that come after this
+    /// node, not previous siblings.
+    pub fn siblings(&self) -> DevTreeIter<'a, 'dt> {
+        let mut i = self.parse_iter.clone();
+        i.limit_to_siblings();
+        i
+    }
+
+    /// Return and iterator over all later sibling nodes of this node.
+    pub fn sibling_nodes(&self) -> DevTreeNodeIter<'a, 'dt> {
+        DevTreeNodeIter(self.siblings_and_descendants())
+    }
+
+    /// Return the next sibling of this node.
+    pub fn next_sibling(&self) -> Result<Option<DevTreeNode<'a, 'dt>>> {
+        self.siblings().next_node()
     }
 }
